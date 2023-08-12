52eb18b0756a17733b7ecb776fc6513ac3d45bb8
hrishabhsharma <sharma17h@gmail.com>
2023-08-12 17:07:34 +0530

    use ladyLoading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import RouteComp from './Project_6/Component/RouteComp'
+import React,{Suspense, lazy} from 'react'
+// import RouteComp from './Project_6/Component/RouteComp'
 // import PageFooter from './Project_6/Component/PageFooter'
 // import HeadNavbar from './Project_6/Component/HeadNavbar'
+
+const LazyRoute = lazy(()=>import('./Project_6/Component/RouteComp'))
 
 const App = () => {
   return (
     <>
     {/* <HeadNavbar/> */}
-    <RouteComp/>
+    <Suspense fallback={<div><h1>Loading ...</h1></div>}>
+      <LazyRoute/>
+    </Suspense>
     {/* <PageFooter/> */}
     </>
   )
