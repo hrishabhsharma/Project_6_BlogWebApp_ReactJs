153dd6ede3b51419c83ae84755553402976d45d5
hrishabhsharma <sharma17h@gmail.com>
2023-09-02 14:13:19 +0530

    new

--- src/Project_6/Component/HeadNavbar.js
+++ src/Project_6/Component/HeadNavbar.js
@@ -20,12 +20,12 @@ const HeadNavbar = () => {
         <img onClick={()=>setToggle(!Toggle)} className='Burger_Icon' width="50" height="50" src="https://img.icons8.com/ios/50/menu--v7.png" alt="icon not found"/>
       </div>
       <div className={Toggle ? 'DropDown' : 'Navbar'}>
-          <NavLink onClick={Dropdown} activeClassName="active" exact to='/'>Home</NavLink>
-          <NavLink onClick={Dropdown} activeClassName="active" exact to='/bollywood'>Bollywood</NavLink>
-          <NavLink onClick={Dropdown} activeClassName="active" exact to='/technology'>Technology</NavLink>
-          <NavLink onClick={Dropdown} activeClassName="active" exact to='/hollywood'>Hollywood</NavLink>
-          <NavLink onClick={Dropdown} activeClassName="active" exact to='/fitness'>Fitness</NavLink>
-          <NavLink onClick={Dropdown} activeClassName="active" exact to='/food'>Food</NavLink>
+          <NavLink onClick={Dropdown} activeclassname="active" to='/'>Home</NavLink>
+          <NavLink onClick={Dropdown} activeclassname="active" to='/bollywood'>Bollywood</NavLink>
+          <NavLink onClick={Dropdown} activeclassname="active" to='/technology'>Technology</NavLink>
+          <NavLink onClick={Dropdown} activeclassname="active" to='/hollywood'>Hollywood</NavLink>
+          <NavLink onClick={Dropdown} activeclassname="active" to='/fitness'>Fitness</NavLink>
+          <NavLink onClick={Dropdown} activeclassname="active" to='/food'>Food</NavLink>
       </div>
     </header>
     </>
